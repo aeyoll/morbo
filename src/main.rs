601557709352ac096f65ab46691958e8172db63c
Jean-Philippe Bidegain <jp@bidega.in>
2021-03-14 11:17:36 +0100

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fmt;
-use tide::Request;
 use tide::prelude::*;
+use tide::Request;
 
 #[derive(Debug, Deserialize)]
 struct CspReport {
@@ -10,20 +10,51 @@ struct CspReport {
 
 #[derive(Debug, Deserialize)]
 struct CspReportContent {
+    /// The URI of the resource that was blocked from loading by the
+    /// Content Security Policy. If the blocked URI is from a different
+    /// origin than the document-uri, then the blocked URI is truncated
+    /// to contain just the scheme, host, and port.
+    #[serde(alias = "blocked-uri")]
+    blocked_uri: String,
+
+    /// Either "enforce" or "report" depending on whether the
+    /// Content-Security-Policy-Report-Only header or
+    /// the Content-Security-Policy header is used.
+    disposition: String,
+
+    /// The URI of the document in which the violation occurred.
     #[serde(alias = "document-uri")]
     document_uri: String,
 
+    /// The directive whose enforcement caused the violation. Some browsers
+    /// may provide different values, such as Chrome providing
+    /// style-src-elem/style-src-attr, even when the actual enforced directive
+    /// was style-src.
+    #[serde(alias = "effective-directive")]
+    effective_directive: String,
+
+    /// The original policy as specified by the Content-Security-Policy
+    /// HTTP header.
+    #[serde(alias = "original-policy")]
+    original_policy: String,
+
+    /// The referrer of the document in which the violation occurred.
     referrer: String,
 
-    #[serde(alias = "blocked-uri")]
-    blocked_uri: String,
+    /// The first 40 characters of the inline script, event handler, or
+    /// style that caused the violation. Only applicable to script-src*
+    /// and style-src* violations, when they contain the 'report-sample'
+    #[serde(alias = "script-sample")]
+    script_sample: String,
+
+    /// The HTTP status code of the resource on which the global object
+    /// was instantiated.
+    #[serde(alias = "status_code")]
+    status_code: String,
 
     /// The name of the policy section that was violated.
     #[serde(alias = "violated-directive")]
     violated_directive: String,
-
-    #[serde(alias = "original-policy")]
-    original_policy: String,
 }
 
 impl fmt::Display for CspReportContent {
