80a1dd3b4c220b470104eb3b089b88838c3bccf9
Jean-Philippe Bidegain <jp@bidega.in>
2021-08-25 19:40:09 +0200

    Add tests

--- src/csp/csp_report_content.rs
+++ src/csp/csp_report_content.rs
@@ -59,41 +59,144 @@ pub struct CspReportContent {
 }
 
 impl CspReportContent {
-    fn is_in_blocked_uri_filters(&self) -> bool {
-        BLOCKED_URI_FILTERS
-            .iter()
-            .any(|x| *x == self.blocked_uri)
+    /// Initialize a CspReport with the required fields
+    pub fn default(
+        blocked_uri: &str,
+        document_uri: &str,
+        original_policy: &str,
+        referrer: &str,
+        violated_directive: &str,
+    ) -> Self {
+        CspReportContent {
+            blocked_uri: String::from(blocked_uri),
+            document_uri: String::from(document_uri),
+            original_policy: String::from(original_policy),
+            referrer: String::from(referrer),
+            violated_directive: String::from(violated_directive),
+            disposition: None,
+            effective_directive: None,
+            script_sample: None,
+            status_code: None,
+            source_file: None,
+        }
     }
 
+    /// Test if a report is in blocked_uri filters
+    pub fn is_in_blocked_uri_filters(&self) -> bool {
+        BLOCKED_URI_FILTERS.iter().any(|x| *x == self.blocked_uri)
+    }
+
+    /// Test if a report is in original policy filters
     fn is_in_original_policy_filters(&self) -> bool {
         ORIGINAL_POLICY_FILTERS
             .iter()
             .any(|x| *x == self.original_policy)
     }
 
+    /// Test if a report is in referrer filters
     fn is_in_referrer_filters(&self) -> bool {
-        REFERRER_FILTERS
-            .iter()
-            .any(|x| *x == self.referrer)
+        REFERRER_FILTERS.iter().any(|x| *x == self.referrer)
     }
 
+    /// Test if a report is in script sample filters
     fn is_in_script_sample_filters(&self) -> bool {
         SCRIPT_SAMPLE_FILTERS
             .iter()
             .any(|x| self.script_sample.is_some() && *x == self.script_sample.as_ref().unwrap())
     }
 
+    /// Test if a report is in original policy filters
     fn is_in_source_file_filters(&self) -> bool {
         SOURCE_FILE_FILTERS
             .iter()
             .any(|x| self.source_file.is_some() && *x == self.source_file.as_ref().unwrap())
     }
 
+    /// Test if a report is in the block list
     pub fn is_in_block_list(&self) -> bool {
         self.is_in_blocked_uri_filters()
             || self.is_in_original_policy_filters()
             || self.is_in_referrer_filters()
             || self.is_in_script_sample_filters()
             || self.is_in_source_file_filters()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_is_in_blocked_uri_filters() {
+        let blocked_uri = "mxjscall://";
+        let csp_c = CspReportContent::default(blocked_uri, "", "", "", "");
+        assert_eq!(true, csp_c.is_in_blocked_uri_filters());
+    }
+
+    #[test]
+    fn it_is_not_in_blocked_uri_filters() {
+        let blocked_uri = "https://fonts.googleapis.com";
+        let csp_c = CspReportContent::default(blocked_uri, "", "", "", "");
+        assert_eq!(false, csp_c.is_in_blocked_uri_filters());
+    }
+
+    #[test]
+    fn it_is_in_original_policy_filters() {
+        let original_policy = ".blackspider.com";
+        let csp_c = CspReportContent::default("", "", original_policy, "", "");
+        assert_eq!(true, csp_c.is_in_original_policy_filters());
+    }
+
+    #[test]
+    fn it_is_not_in_original_policy_filters() {
+        let original_policy = "https://fonts.googleapis.com";
+        let csp_c = CspReportContent::default("", "", original_policy, "", "");
+        assert_eq!(false, csp_c.is_in_original_policy_filters());
+    }
+
+    #[test]
+    fn it_is_in_referrer_filters() {
+        let referrer = "http://l.facebook.com";
+        let csp_c = CspReportContent::default("", "", "", referrer, "");
+        assert_eq!(true, csp_c.is_in_referrer_filters());
+    }
+
+    #[test]
+    fn it_is_not_in_referrer_filters() {
+        let referrer = "https://fonts.googleapis.com";
+        let csp_c = CspReportContent::default("", "", "", referrer, "");
+        assert_eq!(false, csp_c.is_in_referrer_filters());
+    }
+
+    #[test]
+    fn it_is_in_script_sample_filters() {
+        let script_sample = "var BlockAdBlock = function";
+        let mut csp_c = CspReportContent::default("", "", "", "", "");
+        csp_c.script_sample = Some(String::from(script_sample));
+        assert_eq!(true, csp_c.is_in_script_sample_filters());
+    }
+
+    #[test]
+    fn it_is_not_in_script_sample_filters() {
+        let script_sample = "console.log('Rust is great!');'";
+        let mut csp_c = CspReportContent::default("", "", "", "", "");
+        csp_c.script_sample = Some(String::from(script_sample));
+        assert_eq!(false, csp_c.is_in_script_sample_filters());
+    }
+
+    #[test]
+    fn it_is_in_source_file_filters() {
+        let source_file = "chromenull://";
+        let mut csp_c = CspReportContent::default("", "", "", "", "");
+        csp_c.source_file = Some(String::from(source_file));
+        assert_eq!(true, csp_c.is_in_source_file_filters());
+    }
+
+    #[test]
+    fn it_is_not_in_source_file_filters() {
+        let source_file = "fonts.googleapis.com";
+        let mut csp_c = CspReportContent::default("", "", "", "", "");
+        csp_c.source_file = Some(String::from(source_file));
+        assert_eq!(false, csp_c.is_in_source_file_filters());
     }
 }
